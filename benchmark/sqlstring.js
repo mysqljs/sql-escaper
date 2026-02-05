9bc9b9806068d809d6de63a296eb8169152791e6
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2026-02-05 01:41:54 -0300

    chore: checking benchmarks

--- benchmark/sqlstring.js
+++ benchmark/sqlstring.js
@@ -3,4 +3,4 @@ import { cases } from './cases.js';
 
 const { sql, values } = cases[Number(process.env.CASE)];
 
-for (let i = 0; i < 15_000; i++) SqlString.format(sql, values, false);
+for (let i = 0; i < 50_000; i++) SqlString.format(sql, values, false);
